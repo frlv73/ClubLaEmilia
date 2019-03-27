eb03870412252b343293f72a561cc5a13d51030b
Leonel <leovilla_91@hotmail.com>
2019-03-26 21:37:21 -0300

    Proyecto inicial

---
+++ src/main/java/com/proyectofinal/clublaemilia/ClubLaEmiliaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.clublaemilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubLaEmiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubLaEmiliaApplication.class, args);
+	}
+
+}
