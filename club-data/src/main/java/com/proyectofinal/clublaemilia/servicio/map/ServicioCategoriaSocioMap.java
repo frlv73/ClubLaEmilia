08fc02cb8022f6fe388ea26161c851f90ba0e721
Leonel <leovilla_91@hotmail.com>
2019-03-26 23:37:55 -0300

    Implementar servicios basados en HashMaps
    closes #5

---
+++ club-data/src/main/java/com/proyectofinal/clublaemilia/servicio/map/ServicioCategoriaSocioMap.java
@@ -0,0 +1,40 @@
+package com.proyectofinal.clublaemilia.servicio.map;
+
+import java.util.Set;
+
+import com.proyectofinal.clublaemilia.modelo.CategoriaSocio;
+import com.proyectofinal.clublaemilia.servicio.ServicioCategoriaSocio;
+
+public class ServicioCategoriaSocioMap extends AbstractServicioMap<CategoriaSocio, Long> implements ServicioCategoriaSocio {
+
+	@Override
+	public Set<CategoriaSocio> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoriaSocio buscarPorId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoriaSocio guardar(CategoriaSocio objeto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void borrar(CategoriaSocio objeto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void borrarPorId(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ club-data/src/main/java/com/proyectofinal/clublaemilia/servicio/map/ServicioInstalacionMap.java
@@ -0,0 +1,40 @@
+package com.proyectofinal.clublaemilia.servicio.map;
+
+import java.util.Set;
+
+import com.proyectofinal.clublaemilia.modelo.Instalacion;
+import com.proyectofinal.clublaemilia.servicio.ServicioInstalacion;
+
+public class ServicioInstalacionMap extends AbstractServicioMap<Instalacion, Long> implements ServicioInstalacion {
+
+	@Override
+	public Set<Instalacion> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Instalacion buscarPorId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Instalacion guardar(Instalacion objeto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void borrar(Instalacion objeto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void borrarPorId(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
