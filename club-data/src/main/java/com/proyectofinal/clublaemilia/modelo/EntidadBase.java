65b74a278deba972fecdf351daea159bf5850991
Leonel <leovilla_91@hotmail.com>
2019-03-26 23:17:18 -0300

    Agregar ID a los objetos del modelo
    closes #10

---
+++ club-data/src/main/java/com/proyectofinal/clublaemilia/modelo/EntidadBase.java
@@ -0,0 +1,17 @@
+package com.proyectofinal.clublaemilia.modelo;
+
+import java.io.Serializable;
+
+public class EntidadBase implements Serializable{
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
