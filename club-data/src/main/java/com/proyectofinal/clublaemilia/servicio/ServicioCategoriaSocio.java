f1aa9be0aa7bd6be34d0ee3225d6902b157dca11
Leonel <leovilla_91@hotmail.com>
2019-03-26 23:28:15 -0300

    Crear interfaces para servicios
    closes #4

---
+++ club-data/src/main/java/com/proyectofinal/clublaemilia/servicio/ServicioCategoriaSocio.java
@@ -0,0 +1,7 @@
+package com.proyectofinal.clublaemilia.servicio;
+
+import com.proyectofinal.clublaemilia.modelo.CategoriaSocio;
+
+public interface ServicioCategoriaSocio extends ServicioCrud<CategoriaSocio, Long> {
+
+}
---
+++ club-data/src/main/java/com/proyectofinal/clublaemilia/servicio/ServicioInstalacion.java
@@ -0,0 +1,7 @@
+package com.proyectofinal.clublaemilia.servicio;
+
+import com.proyectofinal.clublaemilia.modelo.Instalacion;
+
+public interface ServicioInstalacion extends ServicioCrud<Instalacion, Long> {
+
+}
